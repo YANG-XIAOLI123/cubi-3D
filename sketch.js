2bc16cace2fc19e641bcd294f40c0e0a3afc1317
YANG-XIAOLI123 <yxl980816@gmail.com>
2025-05-06 15:50:30 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,16 @@
+/**
+ * @typedef {object} Cubo
+ * @property {number} x
+ * @property {number} y
+ * @property {number} z
+ * @property {number} size
+ * @property {string} color
+ * @property {function} rotationFunction
+ */
+
+//
+
+/**@type {Cubo[]} */
 let cubi = [];
 let copie = 50;
 function setup() {
@@ -8,7 +21,7 @@ function setup() {
       x: random(-1000, 1000),
       y: random(-1000, 1000),
       z: random(-1000, 1000),
-      size: random(50, 300),
+      size: random(5, 200),
       color: random(["DeepPink", "	MediumOrchid", "MediumSlateBlue"]),
       rotationFunction: random([rotateX, rotateY, rotateZ]),
     };
@@ -24,10 +37,16 @@ function draw() {
   for (let cubo of cubi) {
     push();
     translate(cubo.x, cubo.y, cubo.z);
-    cubo.rotationFunction(frameCount / 20);
+
+    let velocita = frameCount * 0.005;
+
+    cubo.rotationFunction(velocita);
+    rotateZ(velocita);
 
     fill(cubo.color);
-    box(cubo.size);
+    torus(cubo.size, 50);
+    noStroke();
+    //box(cubo.size);
     pop();
   }
 }
