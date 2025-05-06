2a866e311cd8cfa036fcd35effeb6b60b3b861ea
YANG-XIAOLI123 <yxl980816@gmail.com>
2025-05-06 13:08:22 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,35 @@
+let cubi = [];
+let copie = 50;
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, "webgl");
+
+  for (let i = 0; i < copie; i++) {
+    let cubo = {
+      x: random(-1000, 1000),
+      y: random(-1000, 1000),
+      z: random(-1000, 1000),
+      size: 100,
+      color: random(["DeepPink", "	MediumOrchid", "MediumSlateBlue"]),
+      rotationFunction: random([rotateX, rotateY, rotateZ]),
+    };
+
+    cubi.push(cubo);
+  }
 }
 
 function draw() {
-  background(220);
-  const s = 100;
+  background("black");
+  orbitControl();
 
-  rect(0, 0, s, s);
-  rect(0, height - s, s, s);
-  rect(width - s, 0, s, s);
-  rect(width - s, height - s, s, s);
+  for (let cubo of cubi) {
+    push();
+    translate(cubo.x, cubo.y, cubo.z);
+    cubo.rotationFunction(frameCount / 10);
+
+    fill(cubo.color);
+    box(cubo.size);
+    pop();
+  }
 }
 
 function windowResized() {
